721bbe392fedf41ec31ce2153a748facb87d02a5
tybrianna <tybriannahall@gmail.com>
2026-05-20 10:45:44 -0400

    added the ability to search for movies and the details about it inside the move route file

--- movie-finder-api/routes/movieRoutes.js
+++ movie-finder-api/routes/movieRoutes.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+  searchMovies,
+  getMovieDetails,
+} = require("../controllers/movieController");
+
+// Search movies
+router.get("/search", searchMovies);
+
+// Get movie details by IMDb ID
+router.get("/movies/:id", getMovieDetails);
+
+module.exports = router;
